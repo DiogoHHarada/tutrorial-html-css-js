8f10fba6bb897d0528f868e1bada1bd1b7faa208
Diogo Harada <diogohharada@gmail.com>
2025-09-09 20:23:33 -0300

    Criação do botões de editar e excluir via js

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -19,21 +19,39 @@ function addTicker(event){
 
     const card = `
         <div class="card-stock">
-            <header>
-                <h2>${ticker}</h2>
-                <h3>${bolsa}</h3>
-            </header>
-            <main>
-                <span>▲ US$ ${valor}</span>
-            </main>
-            <footer>
-                <span>Ativos: <span>${ativos}</span></span>
-                <span>US$ ${total}</span>
-            </footer>
+            <div class="card-stock" onmouseenter="showButtons(event)" onmouseleave="hideButtons(event)">
+                <header>
+                    <h2>${ticker}</h2>
+                    <h3>${bolsa}</h3>
+                </header>
+                <main>
+                    <span>▲ US$ ${valor}</span>
+                </main>
+                <footer>
+                    <span>Ativos: <span>${ativos}</span></span>
+                    <span>US$ ${total}</span>
+                </footer>
+                <div class="buttons">
+                    <button type="button" onclick="">Editar</button>
+                    <button type="button" onclick="">Excluir</button>
+                </div>
+            </div>
         </div>
     `
 
     const cards = document.getElementById("cards")
     cards.innerHTML += card
     closeModal()
+}
+
+function showButtons(event){
+    const cardStock = event.target
+    const buttons = cardStock.querySelector(".buttons")
+    buttons.style.display = "flex"
+}
+
+function hideButtons(event){
+    const cardStock = event.target
+    const buttons = cardStock.querySelector(".buttons")
+    buttons.style.display = "none"
 }
